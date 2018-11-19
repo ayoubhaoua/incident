b327bbb57884b2124e2499f131c4b09e7ab7e817
ayoubhaoua <dr.ayoubhaoua@gmail.com>
2018-11-19 20:20:28 +0000

    premiere commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {CartographieComponent} from "./cartographie/cartographie.component";
+import {AccueilComponent} from "./accueil/accueil.component";
+import {TableaubordComponent} from "./tableaubord/tableaubord.component";
+import {LoginComponent} from "./login/login.component";
+import {StatistiquesComponent} from "./tableaubord/statistiques/statistiques.component";
+const routes: Routes = [
+  {
+    path:'cartographie',
+    component: CartographieComponent
+  },
+  {
+    path:'accueil',
+    component: AccueilComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'',
+    component: AccueilComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'tableaubord',
+    component: TableaubordComponent,
+    children:[
+      { path: 'stat', component: StatistiquesComponent }
+    ]
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CartographieComponent } from './cartographie/cartographie.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { TableaubordComponent } from './tableaubord/tableaubord.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule} from "@angular/common/http";
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { SidebarComponent } from './tableaubord/sidebar/sidebar.component';
+import { StatistiquesComponent } from './tableaubord/statistiques/statistiques.component';
+import {ChartModule} from "angular-highcharts";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    CartographieComponent,
+    AccueilComponent,
+    TableaubordComponent,
+    LoginComponent,
+    SidebarComponent,
+    StatistiquesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    LeafletModule.forRoot(),
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
